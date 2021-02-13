35f208528ac209ca2e6390bc46b88417f32633c4
ap21029 <78586690+ap21029@users.noreply.github.com>
2021-02-13 14:07:03 +0530

    Add files via upload

---
+++ 3dimenArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { { 1, 0,0 }, { 1, 0, 1 }, { 0, 1, 0 } };
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{array[i, j]}\t");
+                }
+                Console.WriteLine();
+
+            }
+        }
+
+    }
+}
