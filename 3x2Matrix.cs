da107f5025c56c973809954b7e709d449a677e5a
ap21029 <78586690+ap21029@users.noreply.github.com>
2021-02-13 14:08:28 +0530

    Add files via upload

---
+++ 3x2Matrix.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class Class2
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { { 11, 10 }, { 10, 11 }, { 11, 10 } };
+            for (int i = 0; i < 3; i++)//rows i = 3
+            {
+                for (int j = 0; j < 2; j++)//cols j = 0 j= 1 j=2
+                {
+                    Console.Write($"{array[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
